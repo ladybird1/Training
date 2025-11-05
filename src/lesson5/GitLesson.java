51cfbe229be68fbb84b8edc2a3c3fbd79fc5d724
v-lohvyniuk <volodymyr.lohvyniuk@gmail.com>
2025-11-06 00:36:26 +0200

    Add lessons 4 and 5

---
+++ src/lesson5/GitLesson.java
@@ -0,0 +1,12 @@
+package lesson5;
+
+public class GitLesson {
+
+    public static void main(String[] args) {
+
+        System.out.println("This is updated version of the branch");
+
+        System.out.println("dummy update");
+        int a = 4;
+    }
+}
